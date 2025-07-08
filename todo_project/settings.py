74bd6eebcf2aa00819bd3401a67c99abf6e317f7
kavya <kavyasureshm02@gmail.com>
2025-07-09 03:56:45 +0530

    enable console logging for debugging

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -128,3 +128,21 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+# Logging setup to capture errors in production
+import os
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
+
